422bae814e86e0af2947b4ed4ce48d6116c3651f
meike.mertsch@fortum.com <meike.mertsch@fortum.com>
2019-11-30 21:03:58 +0100

    removed outdated stuff

--- test/christmas/day01_test.clj
+++ test/christmas/day01_test.clj
@@ -2,23 +2,4 @@
   (:require [christmas.day01 :as chr]
   		  	[expectations :refer :all]
             [clojure.string :as str]))
-(comment
-(def file (slurp "resources/day01real"))
 
-(expect 3 (chr/exercise01 "1122"))
-(expect 4 (chr/exercise01 "1111"))
-(expect 0 (chr/exercise01 "1234"))
-(expect 9 (chr/exercise01 "91212129"))
-
-(expect 1175 (chr/exercise01 file))
-
-;-----------------------
-
-(expect 6 (chr/exercise01b "1212"))
-(expect 0 (chr/exercise01b "1221"))
-(expect 4 (chr/exercise01b "123425"))
-(expect 12 (chr/exercise01b "123123"))
-(expect 4 (chr/exercise01b "12131415"))
-
-(expect 1166 (chr/exercise01b file))
-)
