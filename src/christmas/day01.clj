c0ef0ebcc7619dabd5f07b5006fc6874e55375f6
meike.mertsch@fortum.com <meike.mertsch@fortum.com>
2019-12-01 06:26:29 +0100

    day01

--- src/christmas/day01.clj
+++ src/christmas/day01.clj
@@ -1,3 +1,29 @@
 (ns christmas.day01
+	(:require [christmas.tools :as tools])
 	(:require [clojure.string :as str]))
 
+(defn parse-file[file]
+	(->> (str/split-lines file)
+		 (map tools/parse-int)
+		 ))
+
+(defn energy-module[value]
+	(-> (/ value 3)
+		Math/floor
+		(- 2)))
+
+(defn total-fuel[values]
+	(->> (map energy-module values)
+		 (apply +)
+		 int))
+
+(defn total-per-module [value]
+	(->> (iterate energy-module  value)
+		 (take-while pos?)
+		 rest
+		 (apply +)))
+
+(defn total-all[values]
+	(->> (map total-per-module values)
+		 (apply +)
+		 int))
--- test/christmas/day01_test.clj
+++ test/christmas/day01_test.clj
@@ -3,4 +3,17 @@
   		  	[expectations :refer :all]
             [clojure.string :as str]))
 
-(def file (slurp "resources/day03real"))
+(comment
+(def file (slurp "resources/day01real"))
+(def values (chr/parse-file file))
+
+(expect "" (chr/parse-file file))
+
+(expect 2.0 (chr/total-per-module 14))
+(expect 966.0 (chr/total-per-module 1969))
+(expect 50346.0 (chr/total-per-module 100756))
+
+(expect 3352674 (chr/total-fuel values))
+(expect 5026151 (chr/total-all values))
+
+)
