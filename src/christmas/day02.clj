08d52897e7492863d5a915572825423066ab9fd3
meike.mertsch@fortum.com <meike.mertsch@fortum.com>
2019-12-02 12:04:31 +0100

    day 02

--- src/christmas/day02.clj
+++ src/christmas/day02.clj
@@ -1,2 +1,64 @@
 (ns christmas.day02
+	(:require [christmas.tools :as tools])
 	(:require [clojure.string :as str]))
+
+(defn parse[file]
+    (->> (re-seq #"\d+" file)
+         (map tools/parse-int)))
+
+(defn do-thing [{:keys [program position] :as game} funct]
+    (let [pos1 (get program (get program (inc position)))
+          pos2 (get program (get program (+ 2 position)))
+          result-pos (get program (+ 3 position))]
+        (update-in game [:program result-pos] (constantly (funct pos1 pos2)))))
+        
+(defn do-end [game]
+    (assoc game :continue false))
+                    
+(defn tick[{:keys [position program] :as game}]
+    (let [instruction (get program position)]
+        (-> (case instruction
+                1 (do-thing game +)
+                2 (do-thing game *)
+                99 (do-end game))
+            (assoc :position (+ 4 position))
+        )))
+
+(defn traverse[program]
+    (->> {:program program :position 0 :continue true}
+         (iterate tick)
+         (take-while :continue)
+         last
+         :program
+         first))
+
+(defn program[file [noun verb]]
+    (-> (update file 1 (constantly noun))
+        (update 2 (constantly verb))
+        traverse))
+
+(defn program1202[file]
+    (->> (parse file)
+         (apply vector)
+         (#(program % [12 2]))))
+
+(defn combine[number]
+    (map #(vector number %) (range 100)))
+
+(defn pairs[]
+    (apply concat (map combine (range 100))))         
+
+(defn thingy[instructions [noun verb :as pair]]
+    (vector (program instructions pair) noun verb))
+    
+(defn program2[file]
+    (let [instructions (apply vector (parse file))]
+        (->> (map #(thingy instructions %) (pairs))
+             (filter #(= 19690720 (first %)))
+             first
+             rest
+             (apply str))))
+
+
+                 
+                 
--- test/christmas/day02_test.clj
+++ test/christmas/day02_test.clj
@@ -3,4 +3,18 @@
   		  	[expectations :refer :all]
             [clojure.string :as str]))
 
-(def file (slurp "resources/day03real"))
+        (comment
+(def file (slurp "resources/day02real"))
+
+(def test1 "1,0,0,0,99")
+(def test2 "2,3,0,3,99")
+(def test3 "2,4,4,5,99,0")
+(def test4 "1,1,1,4,99,5,6,0,99 becomes 30,1,1,4,2,5,6,0,99")
+
+;(expect 6087827 (chr/program1202 file)) ;; 165 too low
+
+
+
+(expect 6087827 (chr/program1202 file)) ;; 165 too low
+(expect "5379" (chr/program2 file)) ;; 165 too low
+        )
