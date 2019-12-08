9e760c13e1c61867a84bf0f6861d9a4fb2466c22
meike.mertsch@fortum.com <meike.mertsch@fortum.com>
2019-12-08 11:45:45 +0100

    day 08

--- src/christmas/day08.clj
+++ src/christmas/day08.clj
@@ -1,2 +1,33 @@
 (ns christmas.day08
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [christmas.tools :as tools]))
+
+(defn parse-layers [file [width length]]
+  (->> (map (comp tools/parse-int str) file)
+       (partition (* width length))))
+
+(defn checksum [file layer-size]
+  (let [layers (parse-layers file layer-size)]
+    (->> (map #(vector (count (filter (partial = 0) %))
+                       (count (filter (partial = 1) %))
+                       (count (filter (partial = 2) %))) layers)
+         (sort-by first)
+         first
+         rest
+         (apply *))))
+
+(def colours {0 " ", 1 "X" nil "."})
+
+(defn draw [picture]
+  (->> (map (partial apply str) picture)
+       (interleave (repeat "\n"))
+       (apply str)
+       (println)))
+
+
+(defn message [file [width _ :as layer-size]]
+  (->> (parse-layers file layer-size)
+       (apply map vector)
+       (map (comp colours first #(drop-while #{2} %)))
+       (partition width)
+       (draw)))
--- test/christmas/day08_test.clj
+++ test/christmas/day08_test.clj
@@ -3,4 +3,25 @@
   		  	[expectations :refer :all]
             [clojure.string :as str]))
 
+(comment
 (def file (slurp "resources/day08real"))
+
+(expect 1360 (chr/checksum file [25 6]))
+
+(expect nil (chr/message file [25 6]))
+)
+
+
+;(        X       X X   X X   X X     X X       X X) 
+;(  X X X X   X X   X   X X   X   X X   X   X X   X) 
+;(      X X   X X   X   X X   X   X X   X   X X   X) 
+;(  X X X X       X X   X X   X         X       X X) 
+;(  X X X X   X X X X   X X   X   X X   X   X   X X) 
+;(  X X X X   X X X X X     X X   X X   X   X X   X)
+
+;XXXX XXX  X  X  XX  XXX
+;X    X  X X  X X  X X  X
+;XXX  X  X X  X X  X X  X
+;X    XXX  X  X XXXX XXX
+;X    X    X  X X  X X X
+;X    X     XX  X  X X  X
